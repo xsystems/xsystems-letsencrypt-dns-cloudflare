b288d3345de92a933d498625b1f67e6ffb4c97a7
Koen Boes <kboes@qualogy.com>
2020-05-26 23:18:31 +0200

    Update release script to facilitate Docker Automated Builds

--- release.sh
+++ release.sh
@@ -9,9 +9,3 @@ COMMIT=${COMMIT:-`git rev-parse HEAD`}
 
 git tag --annotate --message "Release ${VERSION}" ${VERSION} ${COMMIT}
 git push origin ${VERSION}
-
-docker build --tag xsystems/letsencrypt-dns-cloudflare:${VERSION} "https://github.com/xsystems/xsystems-letsencrypt-dns-cloudflare.git#${COMMIT}"
-docker tag xsystems/letsencrypt-dns-cloudflare:${VERSION} xsystems/letsencrypt-dns-cloudflare:latest
-
-docker push xsystems/letsencrypt-dns-cloudflare:${VERSION}
-docker push xsystems/letsencrypt-dns-cloudflare:latest
